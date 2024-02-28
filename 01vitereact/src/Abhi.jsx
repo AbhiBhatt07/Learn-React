46cbadf0ccdb04aa034fce0f0a4bba0a3a3511c4
Abhishek Bhatt <abhibhatt587@gmail.com>
2024-02-28 20:10:31 +0530

    some Practice

---
+++ 01vitereact/src/Abhi.jsx
@@ -0,0 +1,7 @@
+function CallAbhi(){
+    return(
+        <h1>Hello React World</h1>
+    )
+}
+
+export default CallAbhi
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import CallAbhi from "./Abhi";
+
+function App() {
+  return (
+    <>
+      <CallAbhi />
+      <h1 >Hey I started the learning React</h1>
+      <input type="text" placeholder="Hello type here"/>
+    </>
+  )
+}
+
+export default App

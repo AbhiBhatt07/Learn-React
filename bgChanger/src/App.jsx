6d55036da585c3e37de1dbb22a4dd3eb8c996fe3
Abhishek Bhatt <abhibhatt587@gmail.com>
2024-03-05 23:56:40 +0530

    Password Generator Project

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -4,57 +4,68 @@ import "./App.css";
 
 function App() {
   const [color, setColor] = useState();
+  const [color2, setColor2] = useState();
+
+  const changeColor = () => {
+    setColor(`${color}`);
+    setColor2(`${color2}`);
+  };
 
   return (
     <>
       <div
         className="w-full bg-red-400 justify-center flex h-screen"
-        style={{ backgroundColor: color }}
+        // style={{ backgroundColor: color }}
+        style={{
+          backgroundImage: `linear-gradient(to right, ${color}, ${color2})`,
+        }}
       >
-        <h1 className="mt-5 text-[30px] font-semibold h-[100px] p-8 bg-slate-900 text-white">This is The Background changer</h1>
+        <h1 className="mt-5 text-[30px] font-semibold h-[100px] p-8  bg-slate-900 text-white">
+          This is The Background changer
+        </h1>
         <div
           className="flex fixed bottom-7 justify-center flex-wrap px-7 py-5 gap-4 bg-violet-500 
          rounded-2xl text-slate-600 text-2xl "
         >
           <button
-            className="p-3 bg-red-700 rounded-2xl"
-            style={{ backgroundColor: "MistyRose" }}
-            onClick={() => setColor("MistyRose")}
+            className="p-3 bg-gradient-to-r from-purple-500 to-pink-500 rounded-2xl"
+            // onClick={() => setColor("MistyRose")}
+            onClick={changeColor}
           >
             MistyRose
           </button>
           <button
             className="p-3 bg-red-700 rounded-2xl"
-            style={{ backgroundColor: "	Teal" }}
-            onClick={() => setColor("	Teal")}
+            // style={{ backgroundColor: "	Teal" }}
+            onClick={() => setColor("pink, orange")}
           >
             Teal
           </button>
           <button
             className="p-3 bg-red-700 rounded-2xl"
             style={{ backgroundColor: "PowderBlue" }}
-            onClick={() => setColor("PowderBlue")}
+            onClick={() => setColor("PowderBlue, green")}
           >
             PowderBlue
           </button>
           <button
             className="p-3 bg-red-700 rounded-2xl"
             style={{ backgroundColor: "MidnightBlue" }}
-            onClick={() => setColor("MidnightBlue")}
+            onClick={() => setColor("MidnightBlue, brown")}
           >
             MidnightBlue
           </button>
           <button
             className="p-3 bg-red-700 rounded-2xl"
             style={{ backgroundColor: "Brown" }}
-            onClick={() => setColor("Brown")}
+            onClick={() => setColor("Brown, white")}
           >
             Brown
           </button>
           <button
             className="p-3 bg-red-700 rounded-2xl"
             style={{ backgroundColor: "#313131" }}
-            onClick={() => setColor("#313131")}
+            onClick={() => setColor("#313131, yellow")}
           >
             Dark
           </button>
---
+++ randomPassGenrator/src/App.jsx
@@ -0,0 +1,112 @@
+import { useCallback, useState, useEffect, useRef } from "react";
+
+function App() {
+  // this is the variable, function and states
+  const [length, setLength] = useState(8);
+  const [number, setNumber] = useState(false);
+  const [specialChar, setSpecialChar] = useState(false);
+  const [bgcolor, setBgColor] = useState()
+  const [password, setPassword] = useState("cjbvvvjvbvb");
+
+  const passwordRefrence = useRef(null)
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOSPQRSTUVWXYZabcdefgjklmnopqrstuvwxyz";
+    // password genrator logic
+    if (number) str += "0123456789";
+    if (specialChar) str += "!@#$%^&_-=`~*{}[]";
+
+    for (let i = 0; i <= length; i++) {
+      let character = Math.floor(Math.random() * str.length + 1);
+      pass = pass + str.charAt(character);
+      // charAt Hover on and read the work of charAt
+    }
+
+    setPassword(pass);
+  }, [length, number, specialChar, setPassword]);
+
+  const copyToClipBoard = useCallback(() => {
+    passwordRefrence.current?.select();
+    passwordRefrence.current?.setSelectionRange(0, 10)
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, number, specialChar, passwordGenerator]);
+
+  const clickToChange = ()=>{
+    setBgColor('lightBlue')
+  }
+
+  const bothCall = ()=>{
+    copyToClipBoard();
+    clickToChange()
+  }
+  return (
+    <>
+      <div className="bg-black p-5  justify-center flex ">
+        <div className="p-5 bg-gray-600 w-[50%]   rounded-lg mt-20">
+          <div className="flex">
+            <input
+            style={{backgroundColor:bgcolor}}
+              type="text"
+              value={password}
+              readOnly
+              className="w-full p-2 text-2xl font-semibold outline-0 rounded-md bg-stone-500"
+            />
+            <button
+            
+              onClick={bothCall}
+              className="bg-blue-500 p-4 rounded-md text-xl font-bold ms-2"
+            >
+              Copy
+            </button>
+          </div>
+          <input
+            type="range"
+            min={8}
+            max={20}
+            value={length}
+            className="mt-5 ms-3 me-1 cursor-pointer"
+            onChange={(e) => {
+              setLength(e.target.value);
+            }}
+          />
+          <label> Length:{length}</label>
+          <input
+            type="checkbox"
+            defaultChecked={number}
+            className="ms-3 me-1"
+            id="number"
+            onChange={() => {
+              setNumber((previous_Boolean_Value) => {
+                return !previous_Boolean_Value;
+              });
+            }}
+          />
+          <label htmlFor="number">Number</label>
+
+          <input
+            type="checkbox"
+            defaultChecked={specialChar}
+            id="Sc"
+            className="my-3 ms-3 me-1"
+            onChange={() => {
+              // setSpecialChar(
+              //   (previous_Boolean_Value) => !previous_Boolean_Value
+              // );
+              setSpecialChar((previous_Boolean_Value) => {
+                return !previous_Boolean_Value;
+              });
+            }}
+          />
+          <label htmlFor="Sc">Special Character</label>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
